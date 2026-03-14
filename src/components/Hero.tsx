7f2043ae171501459f5fa396c7c431ff1918485b
[dyad] <baraime450@gmail.com>
2026-03-14 22:11:33 +0200

    [dyad] Further reduced top padding in the Hero section to move content closer to the header. - wrote 1 file(s)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ import Scene3D from './Scene3D';
 
 const Hero = () => {
   return (
-    <section id="home" className="min-h-screen flex flex-col justify-start pt-20 lg:pt-24 relative overflow-hidden bg-[#020617]">
+    <section id="home" className="min-h-screen flex flex-col justify-start pt-8 lg:pt-12 relative overflow-hidden bg-[#020617]">
       <Scene3D />
       
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
@@ -19,7 +19,7 @@ const Hero = () => {
             initial={{ opacity: 0, x: -30 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, ease: "easeOut" }}
-            className="lg:col-span-7 space-y-6 pt-4"
+            className="lg:col-span-7 space-y-6"
           >
             <motion.div 
               initial={{ opacity: 0, y: 10 }}
