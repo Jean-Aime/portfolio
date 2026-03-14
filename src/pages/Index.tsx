91a7f6db6ec82976734b8ea692371278b4fb360d
[dyad] <baraime450@gmail.com>
2026-03-14 22:23:07 +0200

    [dyad] Added a Tech Stack marquee and upgraded the Skills section to a modern Bento Grid layout. - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
+import TechStack from '../components/TechStack';
 import About from '../components/About';
 import Experience from '../components/Experience';
 import Education from '../components/Education';
@@ -20,6 +21,7 @@ const Index = () => {
       <Navigation />
       <main>
         <Hero />
+        <TechStack />
         <About />
         <Experience />
         <Education />
